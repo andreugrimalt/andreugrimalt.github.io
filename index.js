317e221dd08bf544a87a126dc919170ed078cbd2
Andreu Grimalt Reynes <andreu@192.168.1.254>
2023-10-21 17:43:58 +0100

    test

--- index.js
+++ index.js
@@ -1,18 +1,8 @@
 const startAudio = async () => {
   const context = new AudioContext();
   await context.audioWorklet.addModule("./worklet.js", { credentials: "omit" });
-  const modulator = new OscillatorNode(context);
-  const modGain = new GainNode(context);
   const noiseGenerator = new AudioWorkletNode(context, "noise-generator");
   noiseGenerator.connect(context.destination);
-
-  // Connect the oscillator to 'amplitude' AudioParam.
-  const paramAmp = noiseGenerator.parameters.get("amplitude");
-  modulator.connect(modGain).connect(paramAmp);
-
-  modulator.frequency.value = 0.5;
-  modGain.gain.value = 0.75;
-  modulator.start();
 };
 
 // add button
